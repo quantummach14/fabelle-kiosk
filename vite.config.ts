71361c29eee20fe7cd0ce87d85207489d3144b6d
tttanurag <anurag.dev@technobubble.co.in>
2025-08-07 13:56:26 +0530

    change in code

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,12 @@
+import { webcrypto } from 'crypto';
+
+if (!globalThis.crypto) {
+  globalThis.crypto = webcrypto as any;
+}
+
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: "/fabellekiosk-frontend/app",
